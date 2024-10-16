c95b5f72cee1b94423ca3a03b7bd406352db8968
Ankur Jaiswal <ankurjaiswalofficial@gmail.com>
2024-10-16 08:19:48 +0000

    added api routes for CRUD tasks

--- crud_operator_app/views.py
+++ crud_operator_app/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
+from .models import Category, Product
+from .serializers import CategorySerializer, ProductSerializer
 
-# Create your views here.
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['name', 'created_at']
+    ordering_fields = ['created_at']
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['name', 'created_at']
+    ordering_fields = ['created_at']
